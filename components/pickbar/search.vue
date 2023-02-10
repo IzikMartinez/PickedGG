80e04547ef179b351302bc0dba9f5b96ce4fe780
isaac <isaacrcm@proton.me>
2023-02-10 12:58:54 -0600

    Vercel didn't like a thing again.

--- components/pickbar/search.vue
+++ components/pickbar/search.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-row absolute left-3">
     <input type="text" id="searchbar" name="searchbar" class="search-bar" v-model="search_text" @focus="search_text = ''" @focusout="search_text='SEARCH...'">
-    <img src="trash.svg" class="w-10 h-10 bg-blue-gray-600 rounded-r-lg p-1.5 cursor-pointer" @click="search_text = 'SEARCH...'">
+    <img src="/trash.svg" class="w-10 h-10 bg-blue-gray-600 rounded-r-lg p-1.5 cursor-pointer" @click="search_text = 'SEARCH...'">
     </div>
 </template>
 
