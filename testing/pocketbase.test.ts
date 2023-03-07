9862d2666ef360b69fee24baeae1eeb263bc1ab0
isaac <isaacrcm@proton.me>
2023-03-07 03:28:13 -0600

    Main card object conversion. Begin bug hunting

--- testing/pocketbase.test.ts
+++ testing/pocketbase.test.ts
@@ -3,9 +3,7 @@ import { usePocketbase, useRecords } from 'composables/usePocketbase'
 
 
 it("PocketBase should have some records", () => {
-    expect(useRecords.at(0)?.id).toBe('l0g474k4t489cv6')
-    console.log(usePocketbase.collection('outsiders').getOne('l0g474k4t489cv6', {
-        expand: 'card_name'
-    })
-    )
+    expect(useRecords!.at(0)?.id).toBe('l0g474k4t489cv6')
+    
+    console.log(useRecords.at(0)!.card_name)
 })
