b1ffacde27dd3d9738a7c2e3520d41b5900e6b6b
isaac <isaacrcm@proton.me>
2023-03-09 17:26:35 -0600

    New backend working.

--- testing/pocketbase.test.ts
+++ testing/pocketbase.test.ts
@@ -3,7 +3,5 @@ import { usePocketbase, useRecords } from 'composables/usePocketbase'
 
 
 it("PocketBase should have some records", () => {
-    expect(useRecords!.at(0)?.id).toBe('l0g474k4t489cv6')
-    
-    console.log(useRecords.at(0)!.card_name)
+    expect(useRecords.length).toBeGreaterThan(0)
 })
